b5d651848210d62128172bbb91d01f9a315c7e0b
Nihat Benli <nihatbenli.nb@gmail.com>
2021-08-20 15:10:32 +0200

    pass all countries to CountriesList component

--- src/components/Countries/index.js
+++ src/components/Countries/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import CountriesList from "../CountriesList";
 import Card from "../UI/Card";
 
+/* 
+!! TODO !!
+Craete filter countries component with filter option 
+and then pass filtered items to <CountriesList items={filteredCountries} /> component  
+*/
+
 const Countries = (props) => {
   return (
     <Card>
-      <CountriesList />
+      <CountriesList items={Countries} />
     </Card>
   );
 };
