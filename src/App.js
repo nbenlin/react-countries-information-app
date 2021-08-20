ee48223fe7d6fd105c505033f773db373dc91fcf
Nihat Benli <nihatbenli.nb@gmail.com>
2021-08-20 15:01:33 +0200

    fetch data of countries

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
+import { useEffect, useState } from "react";
 import Countries from "./components/Countries/";
 
-function App() {
+const App = () => {
+  const [countriesData, setCountriesData] = useState({});
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
+      .then((res) => res.json())
+      .then((data) => data.map((item) => initialize(item)))
+      .catch((err) => console.log("Error:", err.message));
+
+    // A little destructuring...
+    function initialize({
+      name,
+      capital,
+      callingCodes,
+      population,
+      currencies,
+      region,
+    }) {
+      // console.log({
+      //   name,
+      //   capital,
+      //   callingCodes,
+      //   population,
+      //   currencies,
+      //   region,
+      // });
+      setCountriesData({
+        name,
+        capital,
+        callingCodes,
+        population,
+        currencies,
+        region,
+      });
+    }
+  }, []);
+
+  // console.log(countriesData);
   return (
     <>
       <h1>BAE Recruitment Task</h1>
-      <Countries />
+      <Countries items={countriesData} />
     </>
   );
-}
+};
 
 export default App;
