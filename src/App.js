67e10cc6465bb6efb2971a5867f3230a7e42e0de
Nihat Benli <nihatbenli.nb@gmail.com>
2021-08-20 23:07:45 +0200

    add pagination

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import Countries from "./components/Countries/";
 const App = () => {
   const [countriesData, setCountriesData] = useState([]);
 
-  useEffect(() => {
+  const handleFetch = () => {
     fetch("https://restcountries.eu/rest/v2/all")
       .then((res) => res.json())
       .then((data) => setCountriesData(data))
       .catch((err) => console.log("Error:", err.message));
+  };
+
+  useEffect(() => {
+    handleFetch();
   }, []);
 
   return (
--- src/components/CountriesList/index.js
+++ src/components/CountriesList/index.js
@@ -1,28 +1,62 @@
-import React from "react";
-import { List, Fallback } from "../CountriesList/CountriesListElements";
+import React, { useState } from "react";
+import { List, Fallback } from "./CountriesListElements";
 import CountryItem from "../CountryItem";
 
 const CountriesList = (props) => {
+  const { items } = props;
+  const [offset, setOffset] = useState(0);
+  const itemsPerPage = 6; // adjust to get various items per page
+
   if (props.items.length === 0) {
     return <Fallback>Found no countries.</Fallback>;
   }
 
+  const changePage = (offset) => {
+    setOffset(offset);
+  };
+  let listItems = [];
+  for (
+    let i = offset * itemsPerPage;
+    i < offset * itemsPerPage + itemsPerPage;
+    i++
+  ) {
+    if (i >= items.length) {
+      break;
+    }
+    listItems.push(items[i]);
+  }
+
+  listItems = listItems.map((item, idx) => {
+    return (
+      <CountryItem
+        key={`list-${idx}`}
+        id={item.callingCodes}
+        name={item.name}
+        population={item.population}
+        capital={item.capital}
+        region={item.region}
+        subregion={item.subregion}
+        area={item.area}
+        flag={item.flag}
+      />
+    );
+  });
+
+  const btns = [];
+  for (let i = 1; i <= Math.ceil(items.length / itemsPerPage); i++) {
+    const elem = (
+      <span className="pagination-btn" onClick={() => changePage(i - 1)}>
+        {i}
+      </span>
+    );
+    btns.push(elem);
+  }
+
   return (
-    <List>
-      {props.items.map((country) => (
-        <CountryItem
-          id={country.callingCodes}
-          key={country.numericCode}
-          name={country.name}
-          population={country.population}
-          capital={country.capital}
-          region={country.region}
-          subregion={country.subregion}
-          area={country.area}
-          flag={country.flag}
-        />
-      ))}
-    </List>
+    <>
+      <List>{listItems}</List>
+      <div>{btns}</div>
+    </>
   );
 };
 
